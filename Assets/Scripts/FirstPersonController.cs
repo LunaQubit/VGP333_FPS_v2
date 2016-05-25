1aa1203a1dac6d7deca9f924e7b467811f17981e
WeissGz <365162@AIV.EDMC.STU>
2016-05-25 00:55:31 -0700

    FPS_v2

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -1,15 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FirstPersonController : MonoBehaviour {
+[RequireComponent (typeof(CharacterController))]
+public class FirstPersonController : MonoBehaviour 
+{
 
-	// Use this for initialization
-	void Start () {
+    public float movementSpeed = 10.0f;
+    public float mouseSensitivity = 5.0f;
+    public float upDownRange = 60.0f;
+    public float jumpSpeed = 5.0f;
+
+    private float horizontalRotation = 0;
+    private float verticalRotation = 0;
+    private float verticalVelocity = 0;
+
+    CharacterController characterController;
+    
+    void Start () 
+    {
+//        Screen.lockCursor = true;
+        characterController = GetComponent<CharacterController>();
+        
+	}       
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+    
+	void Update () 
+    {   
+        
+        //Rotation
+        horizontalRotation = Input.GetAxis( "Mouse X" ) * mouseSensitivity;
+        transform.Rotate( 0, horizontalRotation, 0 );
+
+        verticalRotation -= Input.GetAxis( "Mouse Y" ) * mouseSensitivity;
+        verticalRotation = Mathf.Clamp( verticalRotation, -upDownRange, upDownRange );
+        Camera.main.transform.localRotation = Quaternion.Euler( verticalRotation, 0, 0 );
+
+        
+
+        //Movement
+        float forwardSpeed = Input.GetAxis( "Vertical" ) * movementSpeed;
+        float sideSpeed = Input.GetAxis( "Horizontal" ) * movementSpeed;
+
+        verticalVelocity += Physics.gravity.y * Time.deltaTime;
+
+        if( characterController.isGrounded && Input.GetButton( "Jump" ) )
+        {
+            verticalVelocity = jumpSpeed;
+
+        }
+
+
+        Vector3 speed = new Vector3( sideSpeed, verticalVelocity, forwardSpeed );
+        speed = transform.rotation * speed;
+        characterController.Move( speed * Time.deltaTime );
+
+       
 	}
 }
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour 
+{
+
+    public Rigidbody bullet;
+    public float speed = 0;
+
+	
+	void Start () 
+    {
+	
+	}
+	
+	
+	void Update () 
+    {
+	    if( Input.GetButtonDown( "Fire1" ) )
+        {
+            Rigidbody instantiatedBullet = Instantiate( bullet, transform.position, transform.rotation ) as Rigidbody;
+            instantiatedBullet.velocity = transform.TransformDirection( new Vector3( 0, 0, speed ) );
+        } 
+	}
+    
+    
+}
